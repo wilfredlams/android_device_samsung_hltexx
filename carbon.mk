4b0fc48e9467d8efd93ad970d51a4ef5a57d7b2a
mattmanwrx <mbloomfield@gmail.com>
2014-01-03 21:48:42 +0000

    Carbonify
    Signed-off-by: mattmanwrx <mbloomfield@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/samsung/hltexx/full_hltexx.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += 
+PRODUCT_NAME=hltexx 
+TARGET_DEVICE=hltexx 
+BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
+
+PRODUCT_DEVICE := hltexx
+PRODUCT_NAME := cm_hltexx
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_hltexx-eng
+add_lunch_combo carbon_hltexx-userdebug
 
