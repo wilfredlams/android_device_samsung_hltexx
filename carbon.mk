e79f453a392e1c69f4ea68adc4cbc3694c5b326c
mattmanwrx <mbloomfield@gmail.com>
2014-01-30 14:08:01 +0000

    hltexx: fix build finger print
    Change-Id: I1179d220c4e34938395a5bd57bb481b4301d6086
    Signed-off-by: mattmanwrx <mbloomfield@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -7,10 +7,11 @@ $(call inherit-product, vendor/carbon/config/common_gsm.mk)
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += 
-PRODUCT_NAME=hltexx 
-TARGET_DEVICE=hltexx 
-BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=hltexx \
+TARGET_DEVICE=hltexx \
+BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys" \
+PRIVATE_BUILD_DESC="hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
 
 PRODUCT_DEVICE := hltexx
 PRODUCT_NAME := carbon_hltexx
